e925a779f07a63e299474ce7a3a9856841d465f5
rsa1791 <prof.renatao@hotmail.com>
2020-12-12 17:47:13 -0300

    Executada Monitorando erros com Sentry.
    close #170.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 
 
 def home(request):
+    # raise ValueError()
     return HttpResponse('<html><body>Olá Django</body></html>')
     # return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
--- pypro/settings.py
+++ pypro/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 import dj_database_url
 from coverage.annotate import os
 from decouple import config, Csv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -175,3 +178,16 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 #
 #     INSTALLED_APPS.append('s3_folder_storage')
 #     INSTALLED_APPS.append('storages')
+
+SENTRY_DSN=config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )

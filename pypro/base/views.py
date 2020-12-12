1d5d8b917243698eb86cc65acdd0e1657e612c56
rsa1791 <prof.renatao@hotmail.com>
2020-12-11 21:02:21 -0300

    Instalada a Django Debug Toolbar.
    close #160.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Olá Django')
+    return HttpResponse('<html><body>Olá Django</body></html>')
+    # return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')

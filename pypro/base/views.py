4f99c9f29abf2bb23506bf7ae5290ba150ddf42a
rsa1791 <prof.renatao@hotmail.com>
2021-01-02 19:15:52 -0300

    Aprendido a Composição de Templates
    close #263.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -3,4 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
-    # return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')

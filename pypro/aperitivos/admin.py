69d560d968209032faf0d583f37c556179d4c6b4
rsa1791 <prof.renatao@hotmail.com>
2021-01-06 20:05:10 -0300

    Introduzido o Admin do Django
    close #290.

--- pypro/aperitivos/admin.py
+++ pypro/aperitivos/admin.py
@@ -6,5 +6,4 @@ from pypro.aperitivos.models import Video
 class VideoAdmin(ModelAdmin):
     list_display = ('titulo', 'slug', 'creation', 'vimeo_id')
     ordering = ('creation',)
-    prepopulated_fields = {'slug':('titulo',)}
-
+    prepopulated_fields = {'slug': ('titulo',)}

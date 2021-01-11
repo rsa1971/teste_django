c89101adcb351bc7c0680b67118e97bd53610a6a
rsa1791 <prof.renatao@hotmail.com>
2021-01-11 11:33:30 -0300

    Mudança realizadas até os 04min15s da Aula Como Criar Relacionamento de Muitos para Um.

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepolated_fields = {'slug': ('titulo',)}
+
+
+@admin.register(Aula)
+class AulaAdmin(OrderedModelAdmin):
+    list_display = ('titulo', 'modulo', 'move_up_down_links')
+    list_filter = ('modulo',)
     prepolated_fields = {'slug': ('titulo',)}
